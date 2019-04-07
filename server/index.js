7bee06ad845c39e98460f234c060870b2ec7dbf5
Christopher Willis <willis.christopher.a@gmail.com>
2019-04-06 22:59:30 -0500

    server with mongo setup

--- server/index.js
+++ server/index.js
@@ -7,8 +7,26 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const startWebServer = () => {
 
-const port = process.env.PORT || 3001;
-  app.listen(port, () => {
-    console.log(`Listening on port:${port}`);
-});
+  // app.use(express.static('public'))
+  app.use(bodyParser.json());
+
+  const port = process.env.DB_PORT || 3001;
+    app.listen(port, () => {
+      console.log(`Listening on port:${port}`);
+  });
+}
+
+const path = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds115154.mlab.com:15154/nuclear-decay-table`
+mongoose.connect(path, {useNewUrlParser: true}).then(
+  () => { 
+    console.log("mongoose connected successfully");
+   
+    startWebServer();
+  },
+  err => {
+    console.log("mongoose did not connect",err);
+   }
+);
+
